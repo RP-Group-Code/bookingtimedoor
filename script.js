80f7f3cdb9a46626c3323bf196426a109ee0cbc2
RP-Group-Code <mhasurungan54@gmail.com>
2024-06-27 07:48:06 +0700

    commit first

---
+++ script.js
@@ -0,0 +1,16 @@
+function goToBooking() {
+  window.location.assign("booking.html");
+}
+function process() {
+  var fullname = document.getElementById("fullnames").value;
+  var package = document.getElementById("package").value;
+  var num_of_people = document.getElementById("num-of-people").value;
+  var totalPayment = package * num_of_people;
+  document.getElementById("total").innerHTML = totalPayment;
+  setTimeout(() => {
+    alert("Terima kasih Telah memesan! "+fullname);
+    window.location.assign("index.html");
+  }, 3000);
+  document.getElementById("pemesanan").value("");
+  console.log(totalPayment);
+}
